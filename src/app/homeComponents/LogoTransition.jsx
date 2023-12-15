e3e806ca81af3359a7aacb9518550ec1756165c7
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2023-12-15 21:25:26 +0530

    logo transition

---
+++ src/app/homeComponents/LogoTransition.jsx
@@ -0,0 +1,14 @@
+"use client"
+import './LogoTransition.css'
+import assets from '@/assets/allImages';
+import Image from 'next/image';
+
+const LogoTransition = ({screenRatio}) => {
+  return (
+    <div className="container relative w-[100vw] h-[100vh] flex justify-center items-center">
+      <Image className="absolute images  w-64" src={assets.MainLogo}  alt="Your Image" />
+    </div>
+  );
+};
+
+export default LogoTransition;
