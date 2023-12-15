3effdee9afab3170d70abe009af74d2e1636b929
aditya-kumar-25 <aditya.pca.25@gmail.com>
2023-12-16 00:00:33 +0530

    fade-in

--- src/app/page.js
+++ src/app/page.js
@@ -2,51 +2,53 @@
 
 import LogoTransition from "./homeComponents/LogoTransition";
 import { useEffect, useState } from "react";
+import "./page.css";
+
 export default function Home() {
-  const [screenRatio, setScreenRatio] = useState(0);
+  const [flag, setFlag] = useState(false);
 
   useEffect(() => {
-    // Function to calculate and log the viewport ratio
-    const calculateViewportRatio = () => {
-      const w = window.innerWidth;
-      const h = window.innerHeight;
-      const ratio = w / h;
-      setScreenRatio(ratio);
-      console.log(`Viewport Width: ${w}px`);
-      console.log(`Viewport Height: ${h}px`);
-      console.log(`Viewport Ratio: ${ratio}`);
+    const timer = () => {
+      setTimeout(() => {
+        setFlag(true);
+      }, 3000);
     };
-
-    calculateViewportRatio();
-    window.addEventListener("resize", calculateViewportRatio);
-    return () => {
-      window.removeEventListener("resize", calculateViewportRatio);
-    };
+    timer();
   }, []);
+
+  window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.getElementById('fadeInDiv').classList.add('active');
+    }, 3100);
+  });
+
   return (
-    <div className="body-background min-h-screen">
-      {/* <LogoTransition screenRatio = {screenRatio}></LogoTransition> */}
+    <div>
+      {flag && (
+        <div id="fadeInDiv" className="body-background min-h-screen fade-in">
+          {/* <LogoTransition screenRatio = {screenRatio}></LogoTransition> */}
 
-      <div>
-        {/* navbar */}
-        <div className="w-full h-10 top-banner flex justify-center text-center items-center ">
-          <marquee direction="right">
-            Crack the code, unlock your dream career
-          </marquee>
-        </div>
-
-        <div className="bg-white flex flex-wrap justify-between items-center">
-        <div>
-            
-        </div>
           <div>
-            <video autoPlay width="500" loop muted>
-              <source src="hero-globe.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+            {/* navbar */}
+            <div className="w-full h-10 top-banner flex justify-center text-center items-center ">
+              <marquee direction="right">
+                Crack the code, unlock your dream career
+              </marquee>
+            </div>
+
+            <div className="bg-white flex flex-wrap justify-between items-center">
+              <div></div>
+              <div>
+                <video autoPlay width="500" loop muted>
+                  <source src="hero-globe.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {!flag && <LogoTransition />}
     </div>
   );
 }
