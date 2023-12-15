f062c84028b3d25c99e9c36942a3fa0246cdb989
aditya-kumar-25 <aditya.pca.25@gmail.com>
2023-12-16 00:57:00 +0530

    reduxSetup

---
+++ src/app/GlobalRedux/store.js
@@ -0,0 +1,11 @@
+'use client';
+
+import { configureStore } from "@reduxjs/toolkit";
+import GlobalStateSlice from "./Features/GlobalStateSlice";
+
+export const store = configureStore({
+    reducer:{
+        GlobalState : GlobalStateSlice,
+    }   
+})
+
