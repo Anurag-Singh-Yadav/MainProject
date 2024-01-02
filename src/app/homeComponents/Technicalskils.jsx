00a0d3ef0c5a5cdc7b2b0578e98d4251b058dd3b
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-01-03 02:57:51 +0530

    technical skills

--- src/app/homeComponents/Technicalskils.jsx
+++ src/app/homeComponents/Technicalskils.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import './Technicalskills.css'
 import IDE from "../technicalskillscomponents/IDE";
+import './TechnicalSkills.css'
 import Concept from "../technicalskillscomponents/Concept";
 function Technicalskils() {
   return (
     <div>
-      <div className="flex justify-center items-center technical1 font-semibold md:font-bold text-primarybtn text-3xl md:text-6xl py-2 md:py-3">Technical Junction</div>
+      <div className="flex justify-center items-center technical1 font-semibold md:font-bold  text-3xl md:text-6xl py-2 md:py-3">Technical Junction</div>
       <div className="technical2 my-2 flex justify-center items-center text-xl md:text-3xl py-2 px-4">Navigating Tech Challenges with Interview Express</div>
-      <div>Embrace the challenges, for in every tech puzzle lies an opportunity to shine. Navigating tech challenges with Interview Express is not just a journey; it's a path to your ultimate success. So, gear up, tackle those complexities, and let your brilliance light the way to a brighter future.</div>
+      <div className="technical3 font-normal text-xl mx-4 pb-4">Embrace the challenges, for in every tech puzzle lies an opportunity to shine. Navigating tech challenges with Interview Express is not just a journey; it's a path to your ultimate success. So, gear up, tackle those complexities, and let your brilliance light the way to a brighter future.</div>
       <div className="grid grid-cols-1 md:grid-cols-2">
         <IDE></IDE>
 
         <div className="md:h-[100%] h-[900px] ml-1 md:ml-3 grid grid-cols-1 gap-2">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 ">
-            <div className="relative bg-light1 rounded-md">
+            <div className="relative bg-dark1 rounded-md hover:cursor-pointer hover:shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
               <Concept
-                bgColor={"light1"}
                 title={"Programming"}
                 icon={"1"}
               ></Concept>
             </div>
-            <div className="relative bg-light2 rounded-md">
+            <div className="relative bg-dark2 rounded-md hover:cursor-pointer hover:shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
               <Concept
-                bgColor={"light2"}
                 title={"Data Structures"}
                 icon={"2"}
               ></Concept>
@@ -30,16 +29,14 @@ function Technicalskils() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 ">
-            <div className="relative bg-light3 rounded-md">
+            <div className="relative bg-dark3 rounded-md hover:cursor-pointer hover:shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
               <Concept
-                bgColor={"light4"}
                 title={"Advanced Topics"}
                 icon={"3"}
               ></Concept>
             </div>
-            <div className="relative bg-light4 rounded-md">
+            <div className="relative bg-dark4 rounded-md hover:cursor-pointer hover:shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
               <Concept
-                bgColor={"light4"}
                 title={"Numbers Theory"}
                 icon={"4"}
               ></Concept>
--- src/app/technicalskillscomponents/Concept.jsx
+++ src/app/technicalskillscomponents/Concept.jsx
@@ -6,7 +6,7 @@ import { RiNumbersFill } from "react-icons/ri";
 
 function Concept({ bgColor, title, icon }) {
   return (
-    <div className={`relative bg-${bgColor} flex justify-between items-center rounded-md`}>
+    <div className={`relative flex justify-between items-center rounded-md`}>
       <div className="absolute top-8 left-[20%] flex justify-center rounded-lg items-center w-[48px] h-[48px] bg-white ">
         {
             icon == 1 && <VscSymbolNamespace className="w-[30px] h-[30px]" />
@@ -21,21 +21,21 @@ function Concept({ bgColor, title, icon }) {
             icon == 4 && <RiNumbersFill className="w-[30px] h-[30px]" />
         }
       </div>
-      <div className="absolute top-24 md:top-20 left-1 ml-2 md:mx-8 text-white"> 
+      <div className="absolute top-24 md:top-20 left-1 ml-2 md:mx-8 text-black md:text-xl font-serif font-bold"> 
         {title}
       </div>
       <div className="absolute top-20 md:top-32 right-3 w-40 h-32">
       {
-            icon == 1 && <VscSymbolNamespace className="w-[100%] h-[100%]"/>
+            icon == 1 && <VscSymbolNamespace className="w-[100%] h-[100%] text-[#79bcbb]"/>
         }
         {
-            icon == 2 && <HiMiniCircleStack className="w-[100%] h-[100%]"/>
+            icon == 2 && <HiMiniCircleStack className="w-[100%] h-[100%] text-[#594434]"/>
         }
         {
-            icon == 3 && <VscVmOutline className="w-[100%] h-[100%]"></VscVmOutline>
+            icon == 3 && <VscVmOutline className="w-[100%] h-[100%] text-[#877342]"></VscVmOutline>
         }
         {
-            icon == 4 && <RiNumbersFill className="w-[100%] h-[100%]"/>
+            icon == 4 && <RiNumbersFill className="w-[100%] h-[100%] text-[#c76f97]"/>
         }
       </div>
     </div>
