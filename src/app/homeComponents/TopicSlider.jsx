bde006bd0297d65c6b34dd04478798906904829c
aditya-kumar-25 <aditya.pca.25@gmail.com>
2024-01-02 23:49:13 +0530

    done

--- src/app/homeComponents/TopicSlider.jsx
+++ src/app/homeComponents/TopicSlider.jsx
@@ -1,176 +1,120 @@
 "use client";
-import React, { useEffect, useState , Component } from 'react'
-import Loading from './Loading';
-import { useSelector } from 'react-redux';
-import './TopicSlider.css'
-import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import { useSelector } from "react-redux";
+import "./TopicSlider.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-function TopicSlider() {
-    const colors = ['dsa' , 'webDevelopment' , 'competitive' , 'appDevelopment' , 'machineLearning' , 'dataScience'];
+import { data } from "./dummyTopicsData";
+import Image from "next/image";
+import assets from "@/assets/allImages";
 
-    // const [data , setData] = useState(null);
+function TopicSlider(props) {
+  const colors = [
+    "dsa",
+    "webDevelopment",
+    "competitive",
+    "appDevelopment",
+    "machineLearning",
+    "dataScience",
+  ];
 
-    const [width, setWidth] = useState(window.innerWidth);
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-    const [cardNumber , setCardNumber] = useState(33)
+  const isLight = useSelector((state) => {
+    return state.GlobalState.isLight;
+  });
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWidth(window.innerWidth);
-          
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      // fetch data
+      // setData(data)
+    };
 
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            if(width > 1500)setCardNumber(22);
-            else if(width > 960)setCardNumber(30)
-            else if(width > 500) setCardNumber(45);
-            else setCardNumber(80);
-        };
-   
-    }, [window.innerWidth]);
-
-    const isLight = useSelector((state) => {
-        return state.GlobalState.isLight;
-    });
-
-    const arr = {
-        dsa : {
-            topics : [
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'Graph',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                }
-            ],
-        },
-        webDevelopment : {
-            topics : [
-                {
-                    title: 'HTML',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'CSS',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'JS',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                }
-            ],
-        },
-        appDevelopment : {
-            topics : [
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                }
-            ],
-        },
-        interviewCrashCourse: {
-            topics : [
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                },
-                {
-                    title: 'Array',
-                    content : 'In computer science, an array is a data structure consisting of a collection of elements (values or variables), of same memory size, each identified by at least one array index or key.',
-                    link : 'www.pornhub.com'
-                }
-            ],
-        },
-    }
-
-    const [data , setData] = useState(arr['dsa'].topics);
-
-    const handleClick = (e) => {
-        let b = document.getElementsByClassName('active-class');
-        for (let i = 0; i < b.length; i++) {
-            b[i].classList.remove('active-class');
-        }
-        e.target.classList.add('active-class');
-    }
+    fetchData();
+  }, []);
 
   return (
-    <div className={isLight ? ' bg-[#65a15b] py-3 my-4' : ' bg-highBlue py-3 my-4'}>
+    <div className={` ${!isLight ? "text-white" : "text-black"} p-3`}>
+      <div className="flex justify-center gap-2 items-center px-3">
+        <div>
+          <p className="text-base sm:text-xl md:text-3xl font-[500] topics-header">
+            Complete your learning journey with InterviewExpress !
+          </p>
+          <p>
+            We are dedicated to provide the most concise, but complete courses
+            for your tech interviews.
+          </p>
+        </div>
+        <Image src={assets.TechImage2} height={200} width={200} />
+      </div>
 
-            <div className={isLight ? `grid grid-cols-4 justify-between px-1 sm:px-2 md:px-3 lg:px-7  font-sans font-bold text-sm sm:text-base md:text-xl bg-[#65a15b]  transition duration-500` : `grid grid-cols-4 justify-between px-1 sm:px-2 md:px-3 lg:px-7  font-sans font-bold text-xl bg-highBlue transition duration-500 text-white`}>
-                <p className=' hover:cursor-pointer hover:bg-yellow-300 py-3 text-center transition duration-700 active-class' onClick={(e) => {setData(arr['dsa'].topics); handleClick(e)}}>DSA</p>
-                <p className=' hover:cursor-pointer hover:bg-yellow-300 py-3 text-center transition duration-700' onClick={(e) => {setData(arr['webDevelopment'].topics); handleClick(e)}}>Web-Development</p>
-                <p className=' hover:cursor-pointer hover:bg-yellow-300 py-3 text-center transition duration-700' onClick={(e) => {setData(arr['appDevelopment'].topics); handleClick(e)}}>App-Development</p>
-                <p className=' hover:cursor-pointer hover:bg-yellow-300 py-3 text-center transition duration-700' onClick={(e) => {setData(arr['interviewCrashCourse'].topics); handleClick(e)}}>Interview crash-course</p>
+      {data.map((obj, index) => {
+        const { name, img, topics } = obj;
+
+        return (
+          <div key={index} className="py-5">
+            <div className="flex justify-center gap-3 items-center overflow-visible p-3">
+
+              <p className="font-semibold text-xl">{name}</p>
+
+              <Image
+                src={img}
+                alt={"Loading.."}
+                width={150}
+                height={150}
+                loading="lazy"
+                className="overflow-hidden rounded-full"
+              />
             </div>
-        {
-            !data && <Loading />
-        }
 
-        {
-            data &&
+            <Carousel
+              responsive={responsive}
+              infinite={true}
+              autoPlay={true}
+              autoPlaySpeed={3000}
+              showDots={false}
+              className="p-2"
+            >
+              {topics.map((item, ind) => {
+                const { title, content, link } = item;
 
-            <div className='flex overflow-hidden sm:overflow-visible '>
-                <Carousel infiniteLoop autoPlay interval={3000}  centerMode={true} showStatus={false} stopOnHover={true} showIndicators={false} centerSlidePercentage={cardNumber} showThumbs={false} width={width > 500 ? '98vw' : '90vw'}>
-                {
-                    data.map((obj, index) => {
-                        
-                        let { title, content } = obj;
-                        let words = content.split(' ');
-                        let a = words.slice(0, 25).join(' ');
-                        if (words.length > 25) {
-                            a += '...';
-                        }
-
-                        return (
-                            <div key={index} className={` flex flex-col gap-3 justify-center items-center rounded-lg m-3 ${colors[index % 6]} p-2`}>
-                                <p className='font-bold'>{title}</p>
-                                <p>{a}</p>
-                                <a className='px-4 py-2 bg-green-500 text-black hover:bg-dimWhite border-2 border-green-500 rounded-lg hover:text-green-500 transition duration-700' to={'/'}>Read more</a>
-                            </div>
-                        );
-                    })
-                }
-                </Carousel>
-            </div>
-        }
-
+                return (
+                  <div
+                    className={`flex flex-col justify-between gap-4 py-3 rounded-md items-center p-2 mx-3 light${index % topics.length + 1} topic-card  transition duration-300`}
+                    key={ind}
+                  >
+                    <p className="text-lg font-semibold">{title}</p>
+                    <p>{content}</p>
+                    <a href={link} className="px-4 py-2 rounded-full border-2 bg-green-700 border-green-700 text-white hover:text-green-700 hover:bg-white transition duration-300">Read more</a>
+                  </div>
+                );
+              })}
+            </Carousel>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
-
-export default TopicSlider
+export default TopicSlider;
--- src/assets/allImages.js
+++ src/assets/allImages.js
@@ -1,11 +1,15 @@
 import ComputerLogo from './computerLogo.png'
 import MainLogo from './mainLogo.png'
 import TextLogo from './textLogo.png'
+import TechImage1 from './techImage1.png'
+import TechImage2 from './techImage2.png'
 
 const assets = {
     ComputerLogo,
     MainLogo,
     TextLogo,
+    TechImage1,
+    TechImage2,
 };
 
 export default assets
