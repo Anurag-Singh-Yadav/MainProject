428336e0d8aed8272db2177aacc456ce68d1d481
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-01-03 13:14:02 +0530

    interview Experiance

---
+++ public/interviewData.js
@@ -0,0 +1,47 @@
+const interviewData = [
+    {
+      name: "John Doe",
+      userPhoto: "john-doe.jpg",
+      company: "Tech Co",
+      linkedin_id: "john-doe-linkedin",
+      created_on: "2022-01-01",
+      selected: false,
+      position: "Software Engineer",
+      round: 1,
+      company_logo:''
+    },
+    {
+      name: "Alice Smith",
+      userPhoto: "alice-smith.jpg",
+      company: "Innovate Corp",
+      linkedin_id: "alice-smith-linkedin",
+      created_on: "2022-01-05",
+      selected: true,
+      position: "Product Manager",
+      round: 2,
+      company_logo:''
+    },
+    {
+      name: "Bob Johnson",
+      userPhoto: "bob-johnson.jpg",
+      company: "Data Solutions",
+      linkedin_id: "bob-johnson-linkedin",
+      created_on: "2022-01-10",
+      selected: false,
+      position: "Data Analyst",
+      round: 1,
+      company_logo:''
+    },
+    {
+      name: "Eva Rodriguez",
+      userPhoto: "eva-rodriguez.jpg",
+      company: "Tech Innovators",
+      linkedin_id: "eva-rodriguez-linkedin",
+      created_on: "2022-01-15",
+      selected: true,
+      position: "UX Designer",
+      round: 3,
+      company_logo:''
+    },
+  ];
+  export default interviewData;
---
+++ src/app/Interviewcomponents/InterviewCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InterviewCard({name, userPhoto, company,linkedin_id,created_on,seleted,position,round}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InterviewCard
