8a63ffc3ba849371b3cc0740b5c8ceb5d37f12ee
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-01-04 00:50:42 +0530

    correction

--- src/app/homeComponents/Technicalskils.jsx
+++ src/app/homeComponents/Technicalskils.jsx
@@ -16,7 +16,7 @@ function Technicalskils() {
         <IDE></IDE>
 
         <div className="md:h-[100%] h-[900px] ml-1 md:ml-3 grid grid-cols-1 gap-2">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
             <div className="relative bg-dark1 rounded-md hover:cursor-pointer hover:shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
               <Concept
                 title={"Programming"}
